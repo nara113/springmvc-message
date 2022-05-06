f456a2acddee3644d3ec17fdbe1556a45c386653
nara113 <0554my@naver.com>
2022-05-06 23:08:51 +0900

    메시지, 국제화 - 웹 애플리케이션에 메시지 적용하기

--- src/test/java/hello/itemservice/messages/MessageSourceTest.java
+++ src/test/java/hello/itemservice/messages/MessageSourceTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
-import java.beans.Transient;
 import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
